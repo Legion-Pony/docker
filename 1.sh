3635ac5ac0270d9a78794fc8c849ce43ec8e8f20
Legion-Pony <egorcbn@gmail.com>
2020-02-22 20:52:32 +0700

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,58 @@
+if [[ $EUID -ne 0 ]]; then
+   echo You are not the ROOT!;
+else
+   echo You are the ROOT!;
+fi
+
+adduser TOM;
+echo "TOM:111" | chpasswd;
+usermod -aG wheel TOM;
+
+echo "What user do you want to modify?";
+
+read name;
+
+until id $name &> /dev/null;
+do
+   echo "Sorry, no such user, enter again";
+   read name;
+done
+
+echo "OK, the user is in the system";
+echo "Choose action: 1 - set a password expiry date";
+echo "               2 - change a command shell";
+echo "               3 - change a home directory";
+
+read choice;
+
+case $choice in
+   [1] ) 
+        echo "Enter a number of days for the expiration date"
+        read days;
+        chage -M $days $name;
+        chage --list $name;;
+
+   [2] ) 
+        echo "What shell do you want to set?";
+        echo "1 - /bin/sh, 2 - /bin/bash, 3 - /sbin/nologin";
+	grep $name /etc/passwd
+        read bro;
+        case $bro in
+            [1] )
+                 usermod -s /bin/sh $name;;
+            [2] )
+                 usermod -s /bin/bash $name;;
+            [3] )
+                 usermod -s /sbin/nologin $name;;
+        esac
+	grep $name /etc/passwd;;
+
+   [3] ) echo "Enter path to new directory:"
+         read path;
+         mkdir $path;
+         usermod -d $path $name;
+         chown -R $name $path;
+         chmod -R ugo+rw $path;
+         echo "New directory is:"
+         eval echo ~$name;;
+esac
